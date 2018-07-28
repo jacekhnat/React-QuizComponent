f5c4627921f71b78881d94afd095056a1442fcfe
jacekhnat <jacekhnat@gmail.com>
2018-07-28 12:05:34 +0200

    Fifth commit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
+  handleClick(button_text){
+    if(button_text === this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler();
+    }
+  };
 
   render() {
     return(
@@ -11,7 +16,7 @@ class QuizQuestion extends Component{
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((a,i) => <QuizQuestionButton key={i} button_text={a} clickHandler={this.handleClick.bind(this)} />)}
           </ul>
         </section>
       </main>
